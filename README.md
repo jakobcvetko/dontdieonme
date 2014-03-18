77c1361cb55574d84505d508845187ad2648dfb3
Jakob <jakob.boss@gmail.com>
2014-03-18 21:08:50 +0100

    Updated: support for multiple URLs

--- README.md
+++ README.md
@@ -3,8 +3,19 @@
 "Don't die on me is" simple Ruby app that makes request to website every 5 minues.
 
 Make sure you add following to environment variables:
- - URL_TO_GET
- - ROLLBAR_ACCESS_TOKEN
+
+ - DELAY (number of seconds between requests, default 100)
+ - URL_0
+ - URL_1
+ - URL_2
+ - URL_3
+ - ... as many as you like, but they have to be continuous, starting with 0
+
+
+### Rollbar
+If you want to use Rollbar for error reporting:
+
+ - ROLLBAR\_ACCESS_TOKEN
  - ROLLBAR_ENDPOINT
 
 ### People responsible
--- app.rb
+++ app.rb
@@ -9,19 +9,25 @@ require 'eventmachine'
 require 'dotenv'
 require 'rollbar/rails'
 
-Dotenv.load unless ENV["ENV"] == "production"
-Rollbar.configuration.access_token = ENV["ROLLBAR_ACCESS_TOKEN"]
-Rollbar.configuration.endpoint = ENV["ROLLBAR_ENDPOINT"]
+def rollbar?
+	ENV["ROLLBAR_ACCESS_TOKEN"] != nil
+end
 
-Rollbar.configuration.environment = ENV["ENV"] ||= "production"
-Rollbar.configuration.framework = "Ruby: 2.0.0"
+if rollbar?
+	Rollbar.configuration.access_token = ENV["ROLLBAR_ACCESS_TOKEN"]
+	Rollbar.configuration.endpoint = ENV["ROLLBAR_ENDPOINT"]
+
+	Rollbar.configuration.environment = "production"
+	Rollbar.configuration.framework = "Ruby: 2.0.0"
+else
+	Dotenv.load
+end
 
 class Watch
 	def initialize url
 		@url = url
 		@log = Logger.new(STDOUT)
-		@log.info "Tracking: #{url}"
-		@log.info "App is up."
+		@log.info "Started tracking: #{url}"
 		is_alive?
 	end
 
@@ -30,23 +36,44 @@ class Watch
 			uri = URI(@url)
 			request = Net::HTTP.get_response(uri)
 			unless request.is_a?(Net::HTTPSuccess)
-				@log.warn "Error executing request"
+				@log.warn "Error executing request (#{@url})"
 				raise "Error executing request"
 			else
-				@log.info "Is alive."
+				@log.info "#{@url} - Is alive."
 			end
 		rescue Exception => e
 			@log.warn "Big error."
-			Rollbar.report_exception(e)
+			Rollbar.report_exception(e) if rollbar?
 		end
 	end
 end
 
-w = Watch.new(ENV["URL_TO_GET"])
+
+# Loading URLs
+watch_urls = []
+$i = 0
+
+until ENV["URL_#{$i}"] == nil
+	watch_urls.push ENV["URL_#{$i}"]
+	$i += 1;
+end
+
+log = Logger.new(STDOUT)
+log.warn "Starting app"
+log.warn "Loaded #{$i} urls."
+
+watches = watch_urls.map{ |w| Watch.new(w)}
+seconds = ENV["DELAY"].to_i
+
+if seconds == 0
+	seconds = 100
+end
 
 EM.run do
-	EM.add_periodic_timer(60*2) do
-		w.is_alive?
+	EM.add_periodic_timer(seconds) do
+		watches.each do |w|
+			w.is_alive?
+		end
 	end
 end
 
